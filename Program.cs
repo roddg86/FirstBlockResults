68df3cbaf75a1836738cb034b5f34dcf796d1aa3
Dmitry <roddg86@gmail.com>
2022-12-03 15:24:13 +0900

    task solved

--- Program.cs
+++ Program.cs
@@ -14,3 +14,37 @@
 ["1234", "1567", "-2", "computer science"] -> ["-2"]
 ["Russian", "Denmark", "Kazan"] -> []```
  */
+
+ string[] arrayString = { "hello", "2", "world", ":-)" };
+//string[] arrayString2 = { "1234", "1567", "-2", "computer science" };
+//string[] arrayString3 = { "Russian", "Denmark", "Kazan" };
+
+// метод проверят массив на длинну элемента не больше 3 символов
+string[] СhekArrayThreeCharacters(string[] array)
+{
+    int count = 0;
+    string[] resultNewArray = new string[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        if (array[i].Length <= 3)
+        {
+            resultNewArray[count] = array[i];
+            count++;
+        }
+    }
+    return resultNewArray;
+}
+
+// метод выводит массив на экран
+void ShowStringArray(string[] array)
+{
+    for (int i = 0; i < array.Length; i++)
+    {
+        Console.Write(array[i] + " ");
+    }
+    Console.WriteLine();
+}
+
+ShowStringArray(arrayString);
+string[] result = СhekArrayThreeCharacters(arrayString);
+ShowStringArray(result);
